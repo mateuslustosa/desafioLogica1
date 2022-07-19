bebc934ca665351400f3a0e879884cedaa751f74
mateuslustosa <mateuslustosa.o@gmail.com>
2022-07-19 17:50:00 -0300

    Desafio 01 completo

---
+++ script.js
@@ -0,0 +1,26 @@
+// // Crie uma variável que receba um nome
+  let nome = "Mateus";
+  console.log(nome);
+
+// // Crie uma variável que recebe um numero
+let num = 5;
+console.log(num);
+
+// // Crie uma variável com o ano atual e subtraia pelo numero que você guardou;
+ let anoAtual = 2022;
+ console.log(anoAtual - num);
+
+// // agora mostre no console o resultado da soma das duas variaveis guardadas anteriormente
+ console.log(anoAtual + num);
+
+// //Mostre no console a mensagem "Estou mexendo no console.log"
+ let mensagem = "Estou mexendo no console.log";
+ console.log("Estou mexendo no console.log");
+
+// //Crie uma variavel chamada quarta-feira e atribua à ela o valor
+//booleano que representa verdadeiro ou falso e mostre a saida no console
+ let quartaFeira = false;
+ console.log(quartaFeira);
+
+// Exiba no console o tipo de dado da variável quartaFeira
+ console.log( typeof quartaFeira);
